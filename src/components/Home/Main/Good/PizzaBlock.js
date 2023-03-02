18d8462e643c4951ca7c10767c065feb2d8fd828
Greg Ivanchuk <gregiv99gmail.com>
2023-03-02 14:38:08 +0200

    hu

--- src/components/Home/Main/Good/PizzaBlock.js
+++ src/components/Home/Main/Good/PizzaBlock.js
@@ -10,11 +10,11 @@ const MyLoader = () => (
     backgroundColor="#d6d6d6"
     foregroundColor="#6e6e6e"
   >
-    <rect x="50" y="187" rx="0" ry="0" width="260" height="30" /> 
-    <circle cx="160" cy="90" r="90" /> 
-    <rect x="51"  y="233" rx="0" ry="0" width="260" height="85" /> 
-    <rect x="51"  y="328" rx="0" ry="0" width="90"  height="27" /> 
-    <rect x="178" y="328" rx="0" ry="0" width="132" height="27" />
+    <rect x="25" y="187" rx="0" ry="0" width="260" height="30" /> 
+    <circle cx="135" cy="90" r="90" /> 
+    <rect x="26"  y="233" rx="0" ry="0" width="260" height="85" /> 
+    <rect x="26"  y="328" rx="0" ry="0" width="90"  height="27" /> 
+    <rect x="153" y="328" rx="0" ry="0" width="132" height="27" />
   </ContentLoader>
 )
 
